92f5e2033fae250400a05172f81734a86d23332b
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-07-28 15:43:40 +0200

    fix HeaderComponent Styles Commit

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -11,15 +11,15 @@ const HeaderComponent = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="flex fixed justify-between items-center w-[100vw] h-[120px] p-5 bg-yellow-100 ">
+    <header className="flex fixed justify-between items-center w-[100vw] h-[120px] p-5 bg-white ">
         <div
         className=
         /* overflow-hidden make avatar rounded like its container */
-        {`relative w-[100px] h-[100px] bg-white rounded-full transition-all duration-500 ease-in-out
+        {`relative flex w-[100px] h-[100px] bg-gray-100 rounded-full transition-all duration-500 ease-in-out
           ${
           isHoverAvatar 
           ? ' border-2 border-black bg-gray-200' 
-          : ''
+          : 'border-white border-2'
           }
         `}
         onMouseEnter={() => setIsHoverAvatar(true)}
@@ -28,17 +28,18 @@ const HeaderComponent = () => {
         <img
           src={avatarDefault}
           alt="MY AVATAR"
-          className={`absolute top-0 left-0 w-full h-full transition-shadow duration-500 ease-linear ${
-            isHoverAvatar ? 'opacity-0' : 'opacity-100'
+          className={`top-0 left-0 w-full h-full transition-shadow duration-500 ease-linear ${
+            isHoverAvatar ? 'hidden' : 'block'
           }`}
         />
         <img
           src={avatarWink}
           alt="MY AVATAR"
-          className={`absolute top-0 left-0 w-full h-full transition-shadow duration-500 ease-linear ${
-            isHoverAvatar ? 'opacity-100' : 'opacity-0'
+          className={`top-0 left-0 w-full h-full transition-shadow duration-500 ease-linear ${
+            isHoverAvatar ? 'block' : 'hidden'
           }`}
         />
+       
       </div>
       <div className="right_menu relative w-[50px] h-[50px] rounded-full transition-all duration-500 ease-linear">
         <img
@@ -54,19 +55,19 @@ const HeaderComponent = () => {
           className={`absolute cursor-pointer transition-opacity duration-500 ease-in-out ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}
         />
         <div
-          className={`absolute top-full right-0 p-1 mt-2 w-[150px] bg-white border rounded shadow-lg transition-opacity duration-300 ease-in-out ${
+          className={`absolute flex items-center top-full right-0 p-1 mt-2 w-[20vw] h-[30vh] shadow-lg border rounded transition-opacity duration-300 ease-in-out ${
             isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
           }`}
         >
-          <ul>
+          <ul className='w-[100%]'>
             <Link to="/">
-              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-100 cursor-pointer">Home</li>
+              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-300 cursor-pointer">Home</li>
             </Link>
             <Link to="/contact">
-              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-100 cursor-pointer">Contact</li>
+              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-300 cursor-pointer">Contact</li>
             </Link>
             <Link to="/aboutme">
-              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-100 cursor-pointer">About Me</li>
+              <li className="px-4 py-3 ease-in-out duration-200 hover:bg-gray-300 cursor-pointer">About Me</li>
             </Link>
           </ul>
         </div>
