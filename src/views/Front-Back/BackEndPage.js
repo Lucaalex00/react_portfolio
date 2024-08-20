8f2c776a7772da560706b5d0362648bd903b36c9
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-08-20 16:03:32 +0200

    Add Frontend & Backend Pages + Styles and Translate Commit

--- src/views/Front-Back/BackEndPage.js
+++ src/views/Front-Back/BackEndPage.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import "./F_B.css"
+import { Link } from 'react-router-dom'
 
 const BackEndPage = () => {
   return (
-    <div>
-      
-    </div>
+      <React.Fragment>
+      <div className="flex w-full items-center justify-center px-10">
+  <div className="card w-full my-[150px] max-w-2xl my-5 p-5 bg-gray-400 bg-opacity-80 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl overflow-auto">
+    <h2 className="text-2xl font-bold mb-5 text-gray-800 text-center">BACK-END</h2>
+    <p className="text-gray-600 text-center text-[1rem]">
+      I began my journey into Backend development with PHP, where I first encountered the world of server-side programming. <br />
+      Through <strong>PHP</strong>, I was introduced to Object-Oriented Programming <strong>(OOP)</strong>, which allowed me to build more structured and scalable applications. <br />
+      Mastering OOP concepts like encapsulation, inheritance, and polymorphism helped me write cleaner, reusable code, making my backend solutions more maintainable and efficient.
+    </p>
+    <br />
+
+    <p>
+      <ul className="p-3 list-disc text-gray-800">
+        <li>
+          <strong>April 2024:</strong> I learned how to connect my backend with databases, working extensively with <strong>MySQL</strong>. <br />
+          This enabled me to manage and manipulate data effectively, bringing dynamic content to life within my web applications.
+        </li>
+        <br />
+        <li>
+        <strong>June 2024:</strong> I delved deeper into modern backend practices, such as using MVC <strong>(Model-View-Controller) </strong>architecture and implementing <strong>RESTful APIs</strong>. <br />
+          This experience allowed me to create more robust applications, separating concerns and ensuring better maintainability.
+        </li>
+        <br />
+        <li>
+        <strong>August 2024:</strong> I also explored <strong>C#</strong> and <strong>.NET</strong>, which opened up new possibilities in enterprise-level development and object-oriented design. <br />
+        This allowed me to broaden my skill set beyond PHP, giving me the ability to work with different frameworks and languages, including the robust .NET ecosystem, making me a more versatile backend developer.
+
+        </li>
+      </ul>
+    </p>
+  </div>
+</div>
+
+   </React.Fragment>
   )
 }
 
--- src/views/Front-Back/FrontEndPage.js
+++ src/views/Front-Back/FrontEndPage.js
@@ -4,44 +4,40 @@ import { Link } from 'react-router-dom'
 const FrontEndPage = () => {
   return (
    <React.Fragment>
-    <div className="flex items-center w-full h-screen justify-center">
-        <div className="card w-full max-w-3xl p-4 bg-gray-400 bg-opacity-80 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
-          <div className='bg_square absolute h-1/3 aspect-square bottom-1 right-1 bg-[#fa9562] z-[-1] bg-opacity-30 rounded-xl'></div>
-          <div className='bg_square absolute h-1/3 aspect-square top-1 left-1 bg-[#fa9562] z-[-1] bg-opacity-30 rounded-xl'></div>
-          <h2 className="text-2xl font-bold mb-5 text-gray-800 text-center">FRONT-END </h2>
-          <p className="text-gray-600 text-center text-[1rem]">Nel Front-end utilizzo linguaggi di Markup e di Stylesheet quali HTML & CSS. <br/>
-          Grazie a loro, ho scoperto l'efficace interazione di una DOM pulita, a creare Web Applications con una eccellente responsiveness e rendere tutta la User Interface (UI) chiara e utile all'utente. <br/>
-          Come linguaggio di programmazione, ho appreso eccellenti competenze in Javascript agli inizi del 2022 e per la quale ho sviluppato diversi <Link to="/projects" className='hover:text-red-800 duration-200 font-bold'>PROGETTI</Link> nel corso di questi anni. 
-          </p>
-          <br/>
+    <div className="flex w-full items-center justify-center px-10">
+      <div className="card w-full my-[150px] max-w-2xl my-5 p-5 bg-gray-400 bg-opacity-80 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl overflow-auto">
+        <h2 className="text-2xl font-bold mb-5 text-gray-800 text-center">FRONT-END</h2>
+        <p className="text-gray-600 text-center text-[1rem]">
+          In the Front-end, I use Markup and Stylesheet languages such as <strong>HTML</strong> & <strong>CSS</strong>. <br />
+          Thanks to them, I discovered the effective interaction of a clean DOM, which helps create Web Applications with excellent responsiveness and make the entire User Interface <strong>(UI)</strong> clear and useful for the user. <br />
+          As for programming languages, I gained excellent skills in <strong>Javascript</strong> at the beginning of 2022, through which I have developed several <Link to="/projects" className="hover:text-red-800 duration-200 font-bold">PROJECTS</Link> over the years.
+        </p>
+        <br />
 
-          <p>
-            <ul className='p-3 list-disc text-gray-800'>
-              <li>
-              Giugno 2022 : ho trovato un corso sulla piattaforma Udemy per l'apprendimento delle basi dell'informatica.
-              <br/>
-              Qui feci i primi passi nell'ambito della programmazione e sbloccai quella giusta consapevolezza da farmi capire ciò che è comune utilizzo di tutti i giorni.
-              </li>
-              <br/>
-              <li>
-              Gennaio 2024 : ho deciso di cominciare il corso intensivo di 6 mesi <Link to="/certificates">"Boolean"</Link>, dove ho aggiornato le mie competenze di tutto il Front-End. <br/>
-              Ho messo mano per la prima volta a VueJS, uno dei framework di javascript più utilizzati, entrando così nell'ottica delle SPA (SINGLE-PAGE-APPLICATION) e delle API REST. <br/>
-              </li>
-              <br/>
-              <li>
-              Maggio 2024 : ho cominciato a lavorare per una azienda di consulenza denominata ALTEN IT con sede a GENOVA (GE) in Liguria.
-              <br/>
-              Qui ho scoperto React.js, framework di Javascript molto utilizzato per la creazione di applicazioni dinamiche senza necessità di ricaricare la pagina, permettendo così maggiore rapidità, flessibilità ed efficienza.
-
-              </li>
-              <br></br>
-              {/* AGGOINGERE ANGULAR */}
-
-
-            </ul>
-          </p>
-        </div>
+        <p>
+          <ul className="p-3 list-disc text-gray-800">
+            <li>
+            <strong>June 2022:</strong> I found a course on the <strong>Udemy</strong> platform to learn the basics of computer science.
+              <br />
+              Here, I took my first steps in programming and unlocked the right awareness to understand what is commonly used every day.
+            </li>
+            <br />
+            <li>
+            <strong>January 2024:</strong> I decided to start the 6-month intensive course <Link to="/certificates"><strong>"BOOLEAN"</strong></Link>, where I updated my skills in all Front-End development. <br />
+              I got my hands on <strong>VueJS</strong> for the first time, one of the most widely used JavaScript frameworks, entering the world of SPAs <strong>(Single-Page Applications)</strong> and <strong>REST APIs</strong>. <br />
+            </li>
+            <br />
+            <li>
+            <strong>May 2024:</strong> I started working for a consultancy company called ALTEN IT, based in GENOVA (GE), Liguria.
+              <br />
+              Here, I discovered <strong>React.js</strong>, a highly used JavaScript framework for creating dynamic applications without the need to reload the page, thus allowing for greater speed, flexibility, and efficiency.
+            </li>
+            <br></br>
+            {/* ADD ANGULAR */}
+          </ul>
+        </p>
       </div>
+    </div>
    </React.Fragment>
   )
 }
